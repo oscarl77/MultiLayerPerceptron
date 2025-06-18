f74d5fc009373277d6d5f1dbf4ce4481304343c2
oscarl77 <oscarlaurens18@gmail.com>
2025-06-18 23:45:11 +0100

    Defined weight initialisation strategies

---
+++ src/initialisers.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def init_random_uniform(input_dim, output_dim, scale=0.1):
+    return np.random.uniform(low=-scale, high=scale, size=(input_dim, output_dim))
+
+def init_xavier_uniform(input_dim, output_dim):
+    limit = np.sqrt(6. / (input_dim + output_dim))
+    return np.random.uniform(low=-limit, high=limit, size=(input_dim, output_dim))
+
+def init_kaiming(input_dim, output_dim):
+    return np.random.randn(output_dim, input_dim) * np.sqrt(2. / input_dim)
+
+INIT_STRATEGIES = {
+    'random_uniform': init_random_uniform,
+    'xavier_uniform': init_xavier_uniform,
+    'kaiming': init_kaiming,
+}
