505d97d2a18a5ddad95927c7ee1dc1b1bb6e8f6e
oscarl77 <oscarlaurens18@gmail.com>
2025-06-19 18:51:11 +0100

    Added a sgd parameter loader function

--- src/utils/parameter_loader.py
+++ src/utils/parameter_loader.py
@@ -1,21 +1,27 @@
-from src.utils.config_loader import load_config
 from src.mlp_utils.activations import ACTIVATIONS
 from src.mlp_utils.initialisers import INIT_STRATEGIES
 
-def load_model_hyperparams():
-    """Load """
-    config = load_config()
-    params = config["HYPERPARAMETERS"]
+def load_model_hyperparams(config):
+    """Load model hyperparams from config file"""
+    params = config["MODEL_PARAMS"]
     hidden_layers = params["HIDDEN_LAYERS"]
     init_func = INIT_STRATEGIES[params["INIT_STRATEGY"]]
     hidden_activation = ACTIVATIONS[params["HIDDEN_ACTIVATION"]]
     output_activation = ACTIVATIONS[params["OUTPUT_ACTIVATION"]]
     return hidden_layers, hidden_activation, output_activation, init_func
 
-def load_training_hyperparams():
-    config = load_config()
-    params = config["HYPERPARAMETERS"]
+def load_training_hyperparams(config):
+    """Load training hyperparams from config file"""
+    params = config["TRAINING_PARAMS"]
     batch_size = params["BATCH_SIZE"]
     epochs = params["EPOCHS"]
     return batch_size, epochs
+
+def load_sgd_hyperparams(config):
+    """Load SGD hyperparams from config file"""
+    params = config["OPTIMISER_PARAMS"]
+    learning_rate = params["LEARNING_RATE"]
+    momentum = params["MOMENTUM"]
+    return learning_rate, momentum
+
 
