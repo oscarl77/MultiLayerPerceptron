69d63bd0e8f5d153397a5e43fae57af9d7fb4307
oscarl77 <oscarlaurens18@gmail.com>
2025-06-19 17:01:54 +0100

    Defined the backward pass

--- src/model.py
+++ src/model.py
@@ -8,7 +8,8 @@ class MultiLayerPerceptron:
         self.input_dim = input_dim # number of features in input data
         self.output_dim = output_dim # number of neurons in output layer
         self.hidden_layers_config = hidden_layers_config # number of neurons per layer
-        self.hidden_activation_fn = hidden_activation_fn # hidden layer activation functions
+        self.hidden_activation_fn = hidden_activation_fn[0] # hidden layer activation function
+        self.d_hidden_activation_fn = hidden_activation_fn[1] # hidden layer activation function derivative
         self.output_activation_fn = output_activation_fn # output layer activation function
 
         # store number of dimensions per layer and number of layers
@@ -20,8 +21,13 @@ class MultiLayerPerceptron:
         self._initialise_params(weight_init_strategy)
 
     def forward(self, X):
+        """
+        Forward pass algorithm of the model.
+        :param X: Initial input array
+        :return: Output array of output layer i.e. the predictions.
+        """
         A_prev = X # Initial input
-        cache = [] # Store (Z, A_prev) for each layer
+        cache = [] # Store (Z_current, A_prev) for each layer
 
         # For all hidden layers
         for i in range(self.num_layers - 1):
@@ -53,6 +59,63 @@ class MultiLayerPerceptron:
         cache.append((Z_output, A_prev))
 
         return AL, cache
+
+    def backward(self, AL, y_batch, cache):
+        """
+        Algorithm for backward pass of model where all gradients of the loss
+        w.r.t the weights and biases in the network are computed for the current_batch.
+        :param AL: predicted labels array from current batch
+        :param y_batch: true labels from current batch
+        :param cache: List of tuples (Z_current, A_prev) of pre activations
+        and outputs for each layer.
+        :return: List of gradients for all weights and biases
+        """
+        gradients = {}
+        # Gradient of loss w.r.t, output layer's pre-activation
+        # As we are using categorical-cross entropy after a softmax activation,
+        # the gradient is simplified
+        dZ_current = AL - y_batch
+
+        # Iterate through layers backwards from last to first hidden layer
+        for layer_idx in reversed(range(1, self.num_layers + 1)):
+            cache_idx = layer_idx - 1
+
+            Z_current, A_prev = cache[cache_idx]
+            W_current = self.parameters[f'W{layer_idx}']
+
+            dA_prev, dW, db = self._compute_linear_gradients(dZ_current, A_prev, W_current)
+
+            gradients[f'W{layer_idx}'] = dW
+            gradients[f'b{layer_idx}'] = db
+
+            # if not the first layer, calculate
+            if layer_idx > 1:
+                Z_prev = cache[cache_idx - 1][0]
+                dZ_current = self.d_hidden_activation_fn(dA_prev, Z_prev)
+
+        return gradients
+
+    @staticmethod
+    def _compute_linear_gradients(dZ, A_prev, W):
+        """Compute gradients for a linear layer during backpropagation.
+        :param dZ: gradient of the loss w.r.t. the pre-activation of the current layer.
+        :param A_prev: output from the previous layer.
+        :param W: weights of the current layer.
+        :return: tuple (dA_prev, dW, db).
+        """
+        batch_size = dZ.shape[0]
+
+        # compute gradient of loss w.r.t. the weights
+        dW = (A_prev.T @ dZ) / batch_size
+
+        # compute gradient of loss w.r.t the biases
+        db = np.sum(dZ, axis=0, keepdims=True) / batch_size
+
+        # compute gradient of loss w.r.t. output of previous layer
+        dA_prev = dZ @ W.T
+
+        return dA_prev, dW, db
+
 
     def _initialise_params(self, strategy: callable):
         """
