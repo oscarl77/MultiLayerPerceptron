c25628397057d7ed06d788a0648c987853888dca
oscarl77 <oscarlaurens18@gmail.com>
2025-06-20 00:09:45 +0100

    Removed accuracy tracking in training

--- src/train.py
+++ src/train.py
@@ -4,11 +4,12 @@ import numpy as np
 from src.model import MultiLayerPerceptron
 from src.mlp_utils.optimisers import SGDOptimiser
 
-from src.data_tools.process_dataset import get_preprocessed_datasets
 from src.utils.config_loader import load_config
-from src.utils.parameter_loader import *
+from src.utils.parameter_loader import load_sgd_hyperparams, load_training_hyperparams, load_model_hyperparams
+from src.utils.logger import log_experiment
+from src.utils.visualisations import plot_losses
+from src.data_tools.process_dataset import get_preprocessed_datasets
 from src.scripts.training_scripts import train_one_epoch, validate_one_epoch
-
 
 def train():
     config = load_config()
@@ -20,9 +21,9 @@ def train():
     (n, c) = y_train.shape
 
     # Retrieve all hyperparameters
-    hidden_layers, hidden_activation, output_activation, init_func = load_model_hyperparams(config)
-    batch_size, epochs = load_training_hyperparams(config)
-    learning_rate, momentum = load_sgd_hyperparams(config)
+    hidden_layers, hidden_activation, output_activation, init_func = load_model_hyperparams()
+    batch_size, epochs = load_training_hyperparams()
+    learning_rate, momentum = load_sgd_hyperparams()
 
     # Initialise SGD optimiser with momentum
     sgd_optimiser = SGDOptimiser(learning_rate, momentum)
@@ -32,19 +33,28 @@ def train():
                                  hidden_activation_fn=hidden_activation, output_activation_fn=output_activation,
                                  weight_init_strategy=init_func)
 
-    # Run training script for the defined number of epochs
-    for epoch in range(epochs):
-        train_acc, train_loss = train_one_epoch(model, sgd_optimiser, x_train, y_train, batch_size)
-        val_acc, val_loss = validate_one_epoch(model, x_val, y_val, batch_size)
 
-        print(f"Epoch: {epoch + 1}, Train loss: {train_loss:.4f}, Validation loss: {val_loss:.4f}")
+    # Run training script for the defined number of epochs
+    # Log training and validation losses
+    train_losses, val_losses = [], []
+    for epoch in range(epochs):
+        train_loss = train_one_epoch(model, sgd_optimiser, x_train, y_train, batch_size)
+        val_loss = validate_one_epoch(model, x_val, y_val, batch_size)
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        if (epoch + 1) % 2 == 0:
+            print(f"Epoch: {epoch + 1}, Train loss: {train_loss:.4f}, Validation loss: {val_loss:.4f}")
+
+    fig = plot_losses(train_losses, val_losses)
+
+    enable_logging = config["LOGGING_SETTINGS"]["ENABLE_LOGGING"]
+    if enable_logging == "True":
+        log_experiment(model, fig)
 
 def set_seed(config):
-    """
-    Set fixed random seeds throughout project for training reproducibility.
-    :param config: dict containing all project configurations
-    """
-    seed = config["TRAINING_PARAMS"]["RANDOM_SEED"]
+    """Set fixed random seeds throughout project for training reproducibility."""
+    seed = config["TRAINING_CONFIG"]["RANDOM_SEED"]
     random.seed(seed)
     np.random.seed(seed)
 
