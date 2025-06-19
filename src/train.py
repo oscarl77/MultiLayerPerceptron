1084849a52adf5918e9ed0395db401c810d7b4cd
oscarl77 <oscarlaurens18@gmail.com>
2025-06-19 18:52:01 +0100

    implemented sgd parameter loading

--- src/train.py
+++ src/train.py
@@ -1,31 +1,47 @@
 import random
 import numpy as np
 
-from src.utils.config_loader import load_config
 from src.model import MultiLayerPerceptron
+from src.mlp_utils.optimisers import SGDOptimiser
+
 from src.data_tools.process_dataset import get_preprocessed_datasets
-from src.utils.parameter_loader import load_model_hyperparams, load_training_hyperparams
+from src.utils.config_loader import load_config
+from src.utils.parameter_loader import *
 from src.scripts.train_one_epoch import train_one_epoch
 
 
 def train():
-    set_seed()
+    config = load_config()
+    set_seed(config)
+
+    # Load in preprocessed data
     x_train, y_train, x_val, y_val = get_preprocessed_datasets(mode="TRAIN")
     (n, d) = x_train.shape
     (n, c) = y_train.shape
-    hidden_layers, hidden_activation, output_activation, init_func = load_model_hyperparams()
-    batch_size, epochs = load_training_hyperparams()
 
+    # Retrieve all hyperparameters
+    hidden_layers, hidden_activation, output_activation, init_func = load_model_hyperparams(config)
+    batch_size, epochs = load_training_hyperparams(config)
+    learning_rate, momentum = load_sgd_hyperparams(config)
+
+    # Initialise SGD optimiser with momentum
+    sgd_optimiser = SGDOptimiser(learning_rate, momentum)
+
+    # Initialise Multi Layer Perceptron
     model = MultiLayerPerceptron(input_dim=d, output_dim=c, hidden_layers_config=hidden_layers,
                                  hidden_activation_fn=hidden_activation, output_activation_fn=output_activation,
                                  weight_init_strategy=init_func)
 
+    # Run training script for the defined number of epochs
     for epoch in range(epochs):
-        train_one_epoch(model, x_train, y_train, batch_size)
+        train_one_epoch(model, sgd_optimiser, x_train, y_train, batch_size, epoch)
 
-def set_seed():
-    config  = load_config()
-    seed = config["HYPERPARAMETERS"]["RANDOM_SEED"]
+def set_seed(config):
+    """
+    Set fixed random seeds throughout project for training reproducibility.
+    :param config: dict containing all project configurations
+    """
+    seed = config["TRAINING_PARAMS"]["RANDOM_SEED"]
     random.seed(seed)
     np.random.seed(seed)
 
