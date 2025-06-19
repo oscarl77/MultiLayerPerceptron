ec3818ee130f24bab61f32f9a6c1b359ff57820e
oscarl77 <oscarlaurens18@gmail.com>
2025-06-19 18:52:29 +0100

    Implemented sgd optimiser for training

--- src/scripts/train_one_epoch.py
+++ src/scripts/train_one_epoch.py
@@ -1,11 +1,12 @@
 import numpy as np
+
 from src.data_tools.process_dataset import generate_batches
 from src.mlp_utils.loss_fns import cross_entropy_loss
 
-def train_one_epoch(model, x_train, y_train, batch_size):
-    epoch_train_loss = 0.0
-    correct_train_predictions = 0
-    num_train_samples_in_epoch = 0
+def train_one_epoch(model, optimiser, x_train, y_train, batch_size, epoch):
+    running_loss = 0.0
+    correct_predictions = 0
+    total_samples = 0
 
     for x_batch, y_batch in generate_batches(x_train, y_train, batch_size=batch_size, shuffle=True):
         n = x_batch.shape[0]
@@ -15,20 +16,21 @@ def train_one_epoch(model, x_train, y_train, batch_size):
 
         # Compute cross-entropy loss
         batch_loss = cross_entropy_loss(predictions, y_batch)
-        epoch_train_loss += batch_loss * n
-        num_train_samples_in_epoch += n
-
-        # Compute training accuracy
-        correct_train_predictions += compute_training_accuracy(predictions, y_batch)
+        running_loss += batch_loss * n
 
         # Backward pass
-        gradients = model.backward(x_batch, y_batch, cache)
+        gradients = model.backward(predictions, y_batch, cache)
 
+        # Update model parameters with SGD
+        parameters = model.get_parameters()
+        optimiser.step(parameters, gradients)
 
-    train_accuracy = correct_train_predictions / num_train_samples_in_epoch * 100
-    avg_train_loss = epoch_train_loss / num_train_samples_in_epoch
-    print(f"Train Loss: {avg_train_loss:.4f}, Train Acc: {train_accuracy:.2f}")
+        correct_predictions += compute_training_accuracy(predictions, y_batch)
+        total_samples += n
 
+    train_accuracy = correct_predictions / total_samples * 100
+    avg_train_loss = running_loss / total_samples
+    print(f"Epoch: {epoch + 1}, Train Loss: {avg_train_loss:.4f}, Train Acc: {train_accuracy:.2f}%")
 
 def compute_training_accuracy(predictions, y_batch):
     predicted_classes = np.argmax(predictions, axis=1)
