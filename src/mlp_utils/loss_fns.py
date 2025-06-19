2ef3175101bab63a5619ef180e5ebd7a14f3f434
oscarl77 <oscarlaurens18@gmail.com>
2025-06-19 17:01:42 +0100

    Added clipping to predicted values.

--- src/mlp_utils/loss_fns.py
+++ src/mlp_utils/loss_fns.py
@@ -2,5 +2,9 @@ import numpy as np
 
 def cross_entropy_loss(y_pred, y_true):
     n = y_pred.shape[0]
-    loss_per_sample = y_true * np.log(y_pred)
+    epsilon = 1e-10
+    # Clip our predicted values to prevent the chance of our loss function
+    # computing log(0) as this would result in an error.
+    y_pred_clipped = np.clip(y_pred, epsilon, 1 - epsilon)
+    loss_per_sample = y_true * np.log(y_pred_clipped)
     return -np.sum(loss_per_sample) / n
