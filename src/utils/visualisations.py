4c6a82ad1418165cf221b929c3d71aae8356f65a
oscarl77 <oscarlaurens18@gmail.com>
2025-06-21 15:10:02 +0100

    Added docstrings

--- src/utils/visualisations.py
+++ src/utils/visualisations.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from src.utils.config_loader import load_config
 
 def plot_losses(train_losses, val_losses):
+    """
+    PLot training and validation losses
+    :param train_losses: List of training losses
+    :param val_losses: List of validation losses
+    :return: The plt figure
+    """
     config = load_config()
     experiment_id = config["LOGGING_SETTINGS"]["EXPERIMENT_ID"]
     epochs = range(len(train_losses))
